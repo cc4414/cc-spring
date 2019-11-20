19def720aa9afb21e9e6079bc079a52ef6810530
cc <707683701@qq.com>
2019-11-20 14:31:51 +0800

    完成sys模块的demo

---
+++ cc-spring-demo/cc-spring-demo-sys/src/main/java/com/example/demo/CcSpringDemoSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcSpringDemoSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcSpringDemoSysApplication.class, args);
+	}
+
+}
