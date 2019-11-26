eef2cabb42b087aebd55985b04270a9651e8f00f
cc4414 <707683701@qq.com>
2019-11-26 20:54:08 +0800

    用户登录时，设置默认所在部门id和名称

--- cc-spring-boot-starter-parent/cc-spring-auth/cc-spring-auth-server/src/main/java/cc/cc4414/spring/auth/service/impl/LocalSysUserServiceImpl.java
+++ cc-spring-boot-starter-parent/cc-spring-auth/cc-spring-auth-server/src/main/java/cc/cc4414/spring/auth/service/impl/LocalSysUserServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import cc.cc4414.spring.auth.model.CcUserDetails;
 import cc.cc4414.spring.auth.service.ISysUserService;
 import cc.cc4414.spring.sys.entity.Authority;
+import cc.cc4414.spring.sys.entity.Dept;
 import cc.cc4414.spring.sys.entity.User;
 import cc.cc4414.spring.sys.service.IAuthorityService;
 import cc.cc4414.spring.sys.service.IUserService;
@@ -40,10 +41,14 @@ public class LocalSysUserServiceImpl implements ISysUserService {
 		CcUserDetails userDetails = new CcUserDetails();
 		BeanUtils.copyProperties(user, userDetails);
 		User userInfo = iUserService.get(user.getId(), false, true);
-		List<String> deptIds = userInfo.getDeptList().stream().map(i -> i.getId()).collect(Collectors.toList());
+		List<Dept> deptList = userInfo.getDeptList();
+		List<String> deptIds = deptList.stream().map(i -> i.getId()).collect(Collectors.toList());
 		userDetails.setDeptIds(deptIds);
-		if (deptIds.size() > 0) {
-			userDetails.setDeptId(deptIds.get(0));
+		if (deptList.size() > 0) {
+			// 如果用户有所在部门，则将第一个用户所在部门的id和名称设置到用户详细信息
+			Dept dept = deptList.get(0);
+			userDetails.setDeptId(dept.getId());
+			userDetails.setDeptName(dept.getName());
 		}
 		return userDetails;
 	}
