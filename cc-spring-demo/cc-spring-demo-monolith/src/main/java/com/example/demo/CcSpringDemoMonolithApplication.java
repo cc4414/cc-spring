1e3e026a4a5236145021e14f2ba728a0b87e86a3
cc4414 <707683701@qq.com>
2019-11-27 20:33:36 +0800

    一个单体应用的完整demo

---
+++ cc-spring-demo/cc-spring-demo-monolith/src/main/java/com/example/demo/CcSpringDemoMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcSpringDemoMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcSpringDemoMonolithApplication.class, args);
+	}
+
+}
