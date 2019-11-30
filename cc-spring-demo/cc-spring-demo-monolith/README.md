49528f2762a759beaffa0acecbc0e24df0944487
cc4414 <707683701@qq.com>
2019-11-30 17:05:22 +0800

    将demo另起一个仓库

--- cc-spring-demo/cc-spring-demo-monolith/README.md
+++
@@ -1,5 +0,0 @@
-## 说明
-导入项目后，请按如下步骤操作
-1. 新建数据库，并将db/sys.sql导入
-2. 修改application.yml中的mysql数据库和redis的配置
-3. 启动项目，并访问[http://127.0.0.1:8080/swagger-ui.html](http://127.0.0.1:8080/swagger-ui.html)
--- cc-spring-demo/cc-spring-demo-mybatis/src/main/java/com/example/demo/service/ITestService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.demo.entity.Test;
-
-public interface ITestService extends IService<Test> {
-
-}
